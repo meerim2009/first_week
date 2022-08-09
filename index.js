fa9752adc404485fd1e11e319779445271708f5d
meerim2009 <77788885+meerim2009@users.noreply.github.com>
2022-08-10 01:15:22 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const spinWords = (phrase) => {
+  console.log(phrase)
+
+  let phraseArray = phrase.split(' ')
+  console.log(phraseArray)
+
+  const reverseString = (string) => {
+    return string.split("").reverse().join("")
+  }
+
+  for (let i = 0; i < phraseArray.length; i++) {
+    if (phraseArray[i].length >= 5) {
+      const word = phraseArray[i]
+      phraseArray[i] = reverseString(word)
+    }
+  }
+
+  return phraseArray.join(' ')
+}
+console.log(spinWords( "Hey fellow warriors" ))
+
